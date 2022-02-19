68f9ad853375cae0f5f5e17b6404b6e2db6aa9b7
FaridHarres <farid.harres@gmail.com>
2022-02-19 13:35:58 +0100

    create Component

---
+++ marvel-quizz/src/Components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='footer-container'>
+            <p>Projet réalisé par Fafa </p>
+            <p>Les icones Wolverines, iron-man, Spiderman, Batman ont été prise depuis le site iconfinder.com</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
